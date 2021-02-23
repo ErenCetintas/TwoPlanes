3281a963add03c98cb0b6b409e31fb7ca891a4ff
ErenCetintas <eren.cet35@gmail.com>
2021-02-23 19:13:37 +0300

    Plane Controls

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovementScript : MonoBehaviour
+{
+    /*public GameObject LeftPlane;
+    public GameObject RightPlane;*/
+
+    public Vector2 Xpos;
+    
+    public bool FirstLaneRightPlane, FirstLaneLeftPlane;
+    public bool LeftPlaneControl;
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+         if(LeftPlaneControl){
+             if(FirstLaneLeftPlane){
+                transform.position = Vector3.Lerp(transform.position,new Vector3(-Xpos.y,transform.position.y,0),.05f);
+             }
+             else{
+                transform.position = Vector3.Lerp(transform.position,new Vector3(-Xpos.x,transform.position.y,0),.05f); 
+             }
+
+         }else{
+             if(FirstLaneRightPlane){
+                transform.position = Vector3.Lerp(transform.position,new Vector3(Xpos.y,transform.position.y,0),.05f);
+             }
+             else{
+                transform.position = Vector3.Lerp(transform.position,new Vector3(Xpos.x,transform.position.y,0),.05f); 
+             }
+         }      
+    }
+
+    public void LeftButton(){
+        if(FirstLaneLeftPlane){
+            FirstLaneLeftPlane=false;
+        }else{
+            FirstLaneLeftPlane=true;
+        }
+    }
+
+    public void RightButton(){
+        if(FirstLaneRightPlane){
+            FirstLaneRightPlane=false;
+        }else{
+            FirstLaneRightPlane=true;
+        }
+    }
+}
+
