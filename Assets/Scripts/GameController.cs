a1cb0fa79a289aafd62eed07fc74a8005a79071f
ErenCetintas <eren.cet35@gmail.com>
2021-02-25 03:13:16 +0300

    plane trail

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -21,7 +21,7 @@ public class GameController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         if(CircleSquare.speed<12){
             CircleSquare.speed += CircleSquare.speed*0.0001f;
