1533240f13b3169e4985e62b95ec71448b8b2466
ErenCetintas <eren.cet35@gmail.com>
2021-02-26 04:09:14 +0300

    endGame code

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour
+{
+
+	public Text scoreText;
+	public Text bestText;
+	// Start is called before the first frame update
+	void Start ()
+	{
+		scoreText.text = "";
+		bestText.text = "";
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+        
+	}
+}
